09a3a68718f4b3b9975331b7f2f6ea9d0196561d
LukmanE22 <unsurlukman@gmail.com>
2022-12-10 10:04:20 +0700

    add some function in WebHelloWorld

--- WebHelloWorld/main.go
+++ WebHelloWorld/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 )
 
@@ -9,15 +10,33 @@ func main() {
 	// Handle requests to the root path
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		// Write the "Hello, world!" message to the response
+		logging(r.RemoteAddr, "{__blank__}");
 		fmt.Fprintln(w, "Hello, world!")
 	})
 
 	http.HandleFunc("/example", func(w http.ResponseWriter, r *http.Request) {
+		logging(r.RemoteAddr, "example");
 		fmt.Fprintln(w, "This Is Another Shot !")
+	})
+
+	// geting all url-path that user give to server
+	http.HandleFunc("/read/", func(w http.ResponseWriter, r *http.Request) {
+		logging(r.RemoteAddr, "read-path");
+		// Get the value of the "path"
+		path := r.URL.Path
+		// Print the value of the "path"
+		fmt.Fprintln(w, "Path :", path)
 	})
 
 	// Start the server on port 8080
 	fmt.Println("> http://localhost:8080/")
 	fmt.Println("> http://localhost:8080/example")
-	http.ListenAndServe(":8080", nil)
+	fmt.Println("> http://localhost:8080/read/what_ever/have_fun")
+	// you can delete 0.0.0.0 for localhost
+	http.ListenAndServe("0.0.0.0:8080", nil)
+}
+
+// this is simple logging, read IP Address and URL parameter
+func logging(ip string, param string) {
+	log.Printf("> ip:%s parameter:%s\n", ip, param)
 }
