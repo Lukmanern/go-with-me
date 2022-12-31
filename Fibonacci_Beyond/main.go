9c8b199b95418de5d49ff5ef673058b065913df6
LukmanE22 <unsurlukman@gmail.com>
2023-01-01 00:52:02 +0700

    add some news project: WordsCounter and Fibonacci_Beyond

---
+++ Fibonacci_Beyond/main.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+// fibonacci is a function that returns a channel
+// that generates the Fibonacci sequence.
+func fibonacci() chan int {
+	// new channel
+	out := make(chan int)
+	go func() {
+		// Defer closing the channel to signal 
+		// to the caller that the sequence is complete
+		defer close(out)
+		for i, j := 0, 1; ; i, j = i+j, i {
+			// Send the next number in the sequence on the channel
+			out <- i
+			// Calculate the next two numbers in the sequence
+			// i, j = i+j, i (its same, look up)
+		}
+	}()
+	// Return the channel to the caller
+	return out
+}
+
+func main() {
+	// Generate (calling) the Fibonacci sequence 
+	// and print it to the console.
+	for n := range fibonacci() {
+		fmt.Println(n)
+		if n > 100 {
+			break
+		}
+	}
+}
---
+++ WordsCounter/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	// Read the input string
+	fmt.Print("Enter a string: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+
+	// Create a map to store the counts of each word
+	wordCounts := make(map[string]int)
+
+	// Split the input string into words
+	words := strings.Fields(input)
+
+	// Iterate through the words and update the count for each word
+	for _, word := range words {
+		wordCounts[word]++
+	}
+
+	// Print the count for each word
+	for word, count := range wordCounts {
+		fmt.Printf("%s: %d\n", word, count)
+	}
+}
