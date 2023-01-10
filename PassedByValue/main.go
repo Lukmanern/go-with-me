8169e3893abea237806b074642269682da4879b2
LukmanE22 <unsurlukman@gmail.com>
2023-01-10 18:11:46 +0700

    add more specific example

--- PassedByValue/main.go
+++ PassedByValue/main.go
@@ -10,6 +10,9 @@ func main() {
 
 	doubleTheSlice(sl)
 	fmt.Println(sl) // 2, 4, 6, 8, 10,
+
+	addOneInBack(sl) // it's doesn't work. why?? you can README
+	fmt.Println(sl) // 2, 4, 6, 8, 10,
 }
 
 // This func doesn't have
@@ -19,4 +22,8 @@ func doubleTheSlice(s []int) {
 		// double the value
 		s[i] *= 2
 	}
+}
+
+func addOneInBack(s []int) {
+	s = append(s, 1)
 }
