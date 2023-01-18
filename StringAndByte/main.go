b156cd33131c625729ae7a8ec41c223687a0b7bf
LukmanE22 <unsurlukman@gmail.com>
2023-01-18 19:07:36 +0700

    fix StringAndByte project (its actually just a note for my self ;D)

--- StringAndByte/main.go
+++ StringAndByte/main.go
@@ -1,1 +1,20 @@
 package main
+
+import "fmt"
+
+func main() {
+	compare()
+	byte_to_string()
+}
+
+func compare() {
+	fmt.Println(`string => "a" :`, "a")
+	fmt.Println(`byte => 'a' :`, 'a')
+}
+
+func byte_to_string() {
+	var b byte = 'b'
+	var bString string = string(b)
+	
+	fmt.Println("String :", bString, "\nByte :", b)
+}
