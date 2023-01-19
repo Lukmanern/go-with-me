677021c9643f07a5b19f72e45e33359d4cd714fb
LukmanE22 <unsurlukman@gmail.com>
2023-01-19 21:04:32 +0700

    update NumberIdentifier problem

--- ImplementationObjectOriented/README.md
+++
@@ -1,3 +0,0 @@
-# Implementation OOP
-
-In Go, structs are used to define user-defined types, which can have methods attached to them. This is a form of object-oriented programming (OOP), although Go **does not have** inheritance and some other OOP features found in other languages, so it will kindly different.
--- NumberIdentifier/main.go
+++ NumberIdentifier/main.go
@@ -6,23 +6,38 @@ import (
 )
 
 func main() {
-	var num int = 112 // Example number
+	var num int = -113 // Example number
+	pos_or_neg(num)
+	odd_or_even(num)
+	is_prime(num)
+}
 
+func pos_or_neg(num int) {
 	// Check if the number is positive
 	if num >= 0 {
 		fmt.Println("The number is positive.")
-	} else {
-		fmt.Println("The number is negative.")
+		return
 	}
 
+	fmt.Println("The number is negative.")
+}
+
+func odd_or_even(num int) {
 	// Check if the number is odd
 	if num%2 != 0 {
 		fmt.Println("The number is odd.")
-	} else {
-		fmt.Println("The number is even.")
+		return
 	}
 
+	fmt.Println("The number is even.")
+}
+
+func is_prime(num int) {
 	// Check if the number is prime
+	if num < 2 {
+		fmt.Printf("The number is not prime. It's lower than 2.")
+		return
+	}
 	var i		  int
 	var isPrime   bool = true
 	var maxFactor int  = int(math.Sqrt(float64(num)))
