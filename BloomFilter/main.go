437880b090760f2c8d39fd624b353b59a30e2bc0
LukmanE22 <unsurlukman@gmail.com>
2023-01-28 10:33:54 +0700

    add comments in BloomFilter

--- BloomFilter/main.go
+++ BloomFilter/main.go
@@ -6,14 +6,18 @@ import (
 	"hash/fnv"
 )
 
+// BloomFilter is a data structure that is
+// used to test whether an element is a member of a set.
+// It is probabilistic, meaning that it may produce false
+// positives, but never false negatives.
 type BloomFilter struct {
-	bits []bool
-	k    uint
-	h    hash.Hash64
+	bits []bool // slice of bits used in the Bloom filter
+	k    uint   // number of hash functions to use
+	h    hash.Hash64 // hash function
 }
 
-// NewBloomFilter creates a new Bloom filter with 
-// the given number of bits and hash functions
+// NewBloomFilter creates a new Bloom filter 
+// with the given number of bits and hash functions
 func NewBloomFilter(m uint, k uint) *BloomFilter {
 	bits := make([]bool, m)
 	h := fnv.New64()
@@ -23,31 +27,46 @@ func NewBloomFilter(m uint, k uint) *BloomFilter {
 // Add adds an element to the Bloom filter
 func (bf *BloomFilter) Add(element []byte) {
 	for i := uint(0); i < bf.k; i++ {
-		bf.h.Reset()
-		bf.h.Write(element)
-		bf.h.Write([]byte(fmt.Sprintf("%d", i)))
-		index := bf.h.Sum64() % uint64(len(bf.bits))
-		bf.bits[index] = true
+		// reset hash
+		bf.h.Reset() 
+		// write element to hash
+		bf.h.Write(element) 
+		// write i to hash
+		bf.h.Write([]byte(fmt.Sprintf("%d", i))) 
+		// get index by taking the mod of the sum of the hash
+		index := bf.h.Sum64() % uint64(len(bf.bits)) 
+		// set the bit at the index to true
+		bf.bits[index] = true 
 	}
 }
 
-// Contains checks if an element 
-// is possibly in the Bloom filter
+// Contains checks if an element is 
+// possibly in the Bloom filter
 func (bf *BloomFilter) Contains(element []byte) bool {
 	for i := uint(0); i < bf.k; i++ {
-		bf.h.Reset()
-		bf.h.Write(element)
-		bf.h.Write([]byte(fmt.Sprintf("%d", i)))
-		index := bf.h.Sum64() % uint64(len(bf.bits))
-		if !bf.bits[index] {
-			return false
+		// reset hash
+		bf.h.Reset() 
+		// write element to hash
+		bf.h.Write(element) 
+		// write i to hash
+		bf.h.Write([]byte(fmt.Sprintf("%d", i))) 
+		// get index by taking the mod of the sum of the hash
+		index := bf.h.Sum64() % uint64(len(bf.bits)) 
+		// if the bit at the index is not set
+		if !bf.bits[index] { 
+			// the element is not in the filter
+			return false 
 		}
 	}
-	return true
+	// all bits are set, element 
+	// is possibly in the filter
+	return true 
 }
 
+
 func main() {
-	// Create a new Bloom filter with 100 bits and 5 hash functions
+	// Create a new Bloom filter with 
+	// 100 bits and 5 hash functions
 	bf := NewBloomFilter(100, 5)
 
 	// Add some elements to the Bloom filter
