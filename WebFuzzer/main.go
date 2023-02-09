891f70b1dab4016dfa26120cdb42ecb5282e92f6
LukmanE22 <unsurlukman@gmail.com>
2023-02-09 13:53:44 +0700

    add comments in WebFuzzer Project

--- WebFuzzer/main.go
+++ WebFuzzer/main.go
@@ -6,6 +6,8 @@ import (
 )
 
 func main() {
+	// Initialize an escapeList 
+	// slice with a list of strings
 	escapeList := []string{
 		"%00",
 		"%0a",
@@ -13,29 +15,49 @@ func main() {
 		"%0d",
 		"\r\t",
 	}
+
+	// Set the target URL
 	target := "http://ajakan.xyz/?data="
 
+	// Loop over the escapeList slice
 	for _, escape := range escapeList {
+		// Concatenate the escape string 
+		// with the target URL
 		url := target + escape
+
+		// Create a new GET request using the url
 		req, err := http.NewRequest("GET", url, nil)
 		if err != nil {
-			fmt.Printf("[ERR] %s\n", url)
+			// If there is an error creating the request,
+			// log an error and continue 
+			// to the next iteration
+			fmt.Printf("[ERR] %s\n ::1", url)
 			continue
 		}
 
+		// Create a new HTTP client
 		client := &http.Client{}
+
+		// Execute the request
 		resp, err := client.Do(req)
 		if err != nil {
-			fmt.Printf("[ERR] %s\n", url)
+			// If there is an error executing the request,
+			// log an error and continue 
+			// to the next iteration
+			fmt.Printf("[ERR] %s\n ::2", url)
 			continue
 		}
+
+		// Defer closing the response body
 		defer resp.Body.Close()
 
+		// Check the status code of the response
 		if resp.StatusCode != 200 {
-			fmt.Printf("[ERR] %s\n", url)
+			// If the status code is not 200, log an error
+			fmt.Printf("[ERR] %s\n ::3", url)
 		} else {
+			// If the status code is 200, log success
 			fmt.Printf("[OK] %s\n", url)
 		}
 	}
-
 }
